ef2d326f5fbe1876ebac06790f67ea6188ea87f1
Jack H Laverick from Strath <jacklaverick@ymail.com>
2023-09-04 13:50:33 +0100

    Feat: Use SANBI ecosystem map.

--- R scripts/saltless/saltless.06 HABITAT TYPES.R
+++ R scripts/saltless/saltless.06 HABITAT TYPES.R
@@ -1,5 +1,5 @@
 
-# Convert NGU classess to 8 StrathE2E habitat types
+# Convert Marine Ecosystem Map into StrathE2E habitats
 
 #### Set up ####
 
@@ -9,40 +9,35 @@ Packages <- c("MiMeMo.tools", "stars")                           # List packages
 lapply(Packages, library, character.only = TRUE)                 # Load packages
 source("./R scripts/@_Region file.R")
 
-domains <- readRDS("./Objects/Domains.rds") %>%                  # Load SF polygons of the MiMeMo model domains
+domains <- readRDS("./Objects/Domains.rds") %>%                  # Load SF polygons of model domain
   st_transform(crs = 4326)                                       # Transform to Lat/Lon to match other objects
 
-sediment <- readRDS("../Sediment/Objects/modelled_sediment.rds") # Import full sediment grid
+MEM <- st_read("./Data/MEM18/") # Import South African map
 
-translate <- read.csv("./Data/Sediment habitats.csv") %>%        # Import sediment aggregations
-  mutate(Sed_class = as.factor(SEDKORNSTR)) %>% 
-  select(Sed_class, Habitat)                                     # Drop excess columns
+#### Limit the map to the model domain ####
 
-#### Define geographic extent of each habitat type ####
-
-habitats <- left_join(sediment, translate) %>%                   # Attach habitat labels to predicted NGU classes
-  mutate(Sed_class = as.factor(Sed_class),                       # Convert to factors
-         Habitat = as.factor(Habitat)) %>% 
-  sfc_as_cols()                                                  # Get coordinates from sf formatting to define a raster
-
-numeric_habitats <- mutate(habitats, Habitat = as.numeric(Habitat)) # Convert factor to numeric as st_rasterize expects numbers
-
-polygons <- st_rasterize(numeric_habitats["Habitat"],            # Rasterize habiat labels   
-                         nx = length(unique(habitats$x)),        # At the resolution of the original data
-                         ny = length(unique(habitats$y))) %>% 
-  st_as_sf(aspoints = FALSE, merge = TRUE) %>%                   # Merge pixels into contiguous polygons
-  mutate(Habitat = factor(Habitat, labels = levels(habitats$Habitat))) %>% # Reinstate labels for factor
-  group_by(Habitat) %>%  
-  summarise(Habitat = Habitat[1])                                # Combine polygons into a single row per habitat
-
-plot(polygons)
-
-polygons <- st_intersection(st_make_valid(st_transform(polygons, crs = crs)), # Split sediment polygons along model zones
+polygons <- st_intersection(st_make_valid(st_transform(MEM, crs = crs)), # Split sediment polygons along model zones
                             st_transform(domains, crs = crs)) %>% 
   select(-c(Elevation, area)) %>%                                # Drop excess data
-  st_transform(crs = 4326)                                       # Switch back to mercator
+  st_transform(crs = 4326) %>%                                   # Switch back to mercator
+  mutate(Habitats = case_when(str_detect(Substratum, "Mud") ~ "Mud",
+                              str_detect(Substratum, "Sand") ~ "Sand",
+                              str_detect(Substratum, "Rock") ~ "Rock",
+                              T ~ "unclassified"))
 
-saveRDS(polygons, "./Objects/Habitats.rds")
+ggplot(polygons) +
+  geom_sf(aes(fill = Substratum)) +
+  geom_sf(data = domains, fill = NA, colour = "red")
+  
+polygons <- group_by(polygons, Shore, Habitats) %>% 
+  summarise(Shore = Shore[[1]],
+            Habitats = Habitats[[1]]) %>%  
+  ungroup()
+  
+ggplot(polygons) +
+  geom_sf(aes(fill = Habitats)) 
+
+#saveRDS(polygons, "./Objects/Habitats.rds")
 
 #### Calculate proportion of model zones in each habitat ####
 
@@ -50,9 +45,9 @@ proportions <- polygons %>%
   mutate(Cover = as.numeric(st_area(.))) %>%                     # Measure the area of each habitat type
   st_drop_geometry() %>%                                         # Drop SF formatting
   mutate(Cover = Cover/sum(Cover)) %>%                           # Calculate the proportion of the model zone in each sediment polygon 
-  rename(Bottom = Habitat)
+  rename(Bottom = Habitats)
 
-saveRDS(proportions, "./Objects/Sediment area proportions.rds")
+#saveRDS(proportions, "./Objects/Sediment area proportions.rds")
 
 ggplot(proportions) +
   geom_col(aes(x = Shore, y = Cover*100, fill = Bottom), position = "Dodge") +
