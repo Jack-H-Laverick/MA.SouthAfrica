6a0d7cf38cf9ba294dc128ed33b5378407d93883
Ben Grier <benjamin.grier@strath.ac.uk>
2025-06-05 18:04:26 +0100

    Add message if raster data is already processed previously

--- R scripts/fish/formatting_spatial_effort.R
+++ R scripts/fish/formatting_spatial_effort.R
@@ -14,9 +14,13 @@ directories <- list.dirs("../../Spatial Data/fishing_effort_data/")
 for (directory in directories) {
     files <- list.files(directory, full.names = TRUE)
     if (!any(str_detect(files, ".tif$"))) next
-    if (any(str_detect(files, glue("{crs}.tif")))) next
 
     var_name <- last(str_split(directory, "(/)")[[1]])
+
+    if (any(str_detect(files, glue("{crs}.tif")))) {
+        print(glue("{var_name} already processed"))
+        next
+    }
 
     print(glue("Reprojecting spatial effort data for {var_name}"))
 
