82552dac5aa182a32119b062aabb0d3a0cd0fe52
Ben Grier <benjamin.grier@strath.ac.uk>
2025-06-05 13:44:09 +0100

    Add explorations of Seas Around Us data uncertainty scores

--- R scripts/fish/seas_around_us.R
+++ R scripts/fish/seas_around_us.R
@@ -176,7 +176,7 @@ strath_e2e_gear_landings <- landings %>%
         .default = "other gears"
     )) %>%
     filter(!is.na(Guild) & year >= start_year & year <= end_year)
-
+arrow::write_parquet(strath_e2e_gear_landings, "./Objects/sau_landings_strath_gears.parq")
 
 guild_gear_catch <- strath_e2e_gear_landings %>%
     group_by(gear_type_se2e, Guild) %>%
@@ -281,3 +281,40 @@ ggplot() +
     ) +
     theme_minimal() +
     labs(y = "proportion_of_landings")
+
+# Seas Around Us uncertainty scores
+## For each gear type
+strath_e2e_gear_landings$uncertainty_score <- as.factor(strath_e2e_gear_landings$uncertainty_score)
+strath_e2e_gear_landings$uncertainty_score <- ifelse(is.na(strath_e2e_gear_landings$uncertainty_score), "NA", strath_e2e_gear_landings$uncertainty_score)
+year_gear_total_landings <- strath_e2e_gear_landings %>%
+    group_by(year, gear_type_se2e) %>%
+    summarise(year_sum_tonnes = sum(tonnes))
+sau_landings_uncertain_gear <- strath_e2e_gear_landings %>%
+    group_by(year, uncertainty_score, gear_type_se2e) %>%
+    summarise(tonnes = sum(tonnes)) %>%
+    left_join(., year_gear_total_landings, by = c("year", "gear_type_se2e")) %>%
+    mutate(proportion_catch_uncertainty = tonnes / year_sum_tonnes)
+
+ggplot() +
+    geom_col(
+        data = sau_landings_uncertain_gear,
+        aes(x = year, y = proportion_catch_uncertainty, fill = as.factor(uncertainty_score))
+    ) +
+    facet_wrap(~gear_type_se2e)
+
+## For each Guild and gear type
+year_guild_total_landings <- strath_e2e_gear_landings %>%
+    group_by(year, Guild) %>%
+    summarise(year_sum_tonnes = sum(tonnes))
+sau_landings_uncertain_guild <- strath_e2e_gear_landings %>%
+    group_by(year, uncertainty_score, Guild) %>%
+    summarise(tonnes = sum(tonnes)) %>%
+    left_join(., year_guild_total_landings, by = c("year", "Guild")) %>%
+    mutate(proportion_catch_uncertainty = tonnes / year_sum_tonnes)
+
+ggplot() +
+    geom_col(
+        data = sau_landings_uncertain_guild,
+        aes(x = year, y = proportion_catch_uncertainty, fill = uncertainty_score)
+    ) +
+    facet_wrap(~Guild)
