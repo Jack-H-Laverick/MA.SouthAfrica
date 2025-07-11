6e97309cb7d307aa19b7b21c20f96305f2012769
Ben Grier <benjamin.grier@strath.ac.uk>
2025-07-11 15:34:41 +0100

    Add outputting of mean and standard deviation target data landings values and woa nitrate values

--- R scripts/fish/9_target_data.R
+++ R scripts/fish/9_target_data.R
@@ -29,4 +29,43 @@ target_data <- target_data %>%
 
 target_data[target_data$Name == "Obs_KelpP", ]$Units <- "gC/m2/y"
 
+# Assign values from woa extracted nitrate concentrations to target data slots
+woa_nitrate <- read.csv("./Objects/woa23_nitrate_concentrations.csv") %>%
+    mutate(mnths = if_else(season == "winter", "NDJF", "MJJA")) %>%
+    mutate(dl = if_else(depth == "deep", "d", "s")) %>%
+    mutate(Name = glue("Obs_{mnths}_{dl}_nitrate")) %>%
+    rename(Annual_measure = mean_conc, SD_of_measure = std_conc) %>%
+    select(Annual_measure, SD_of_measure, Name)
+
+target_data <- rows_update(target_data, woa_nitrate, by = "Name")
+
+landings_target <- read.csv("./Objects/guild_landings_target_data.csv") %>%
+    mutate(guild_short = case_when(
+        Guild_code == "CSB" ~ "Bc",
+        Guild_code == "FDB" ~ "Bs",
+        Guild_code == "DF" ~ "D",
+        Guild_code == "KP" ~ "K",
+        Guild_code == "MF" ~ "M",
+        Guild_code == "PF" ~ "P",
+        Guild_code == "CZ" ~ "Zc"
+    )) %>%
+    mutate(Name = glue("Obs_{guild_short}land_livewt")) %>%
+    mutate(Source = "Seas_Around_Us", Time_period = "2010-2019") %>%
+    rename(Annual_measure = annual_average_landings_mMN, SD_of_measure = annual_std_landings_mMN) %>%
+    select(Annual_measure, SD_of_measure, Name, Time_period, Source)
+
+target_data <- rows_update(target_data, landings_target, by = "Name")
+
+discard_target <- read.csv("./Objects/guild_discards_target_data.csv") %>%
+    mutate(guild_name = case_when(
+        Guild_code == "BD" ~ "bird",
+        Guild_code == "SL" ~ "seal",
+        Guild_code == "CT" ~ "ceta"
+    )) %>%
+    mutate(Name = glue("Obs_{guild_name}disc")) %>%
+    mutate(Source = "Seas_Around_Us", Time_period = "2010-2019", Region = "Southern_Benguela") %>%
+    rename(Annual_measure = annual_average_discard_mMN, SD_of_measure = annual_std_discard_mMN) %>%
+    select(Annual_measure, SD_of_measure, Name, Time_period, Source, Region)
+target_data <- rows_update(target_data, discard_target, by = "Name")
+
 write.csv(target_data, glue("./Objects/annual_observed_{domain_name}_{start_year}-{end_year}.csv"), row.names = FALSE)
